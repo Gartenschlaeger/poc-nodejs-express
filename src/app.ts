1334e30c87680c0dbf691dfec33fd5e1d3dc18e8
Gartenschlaeger <mail@kaisnet.de>
2021-10-31 11:58:28 +0100

    replaced processenv with dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import { processenv } from 'processenv';
+import * as dotenv from 'dotenv';
 
 import * as api from './api';
 
-const port = <number>processenv('PORT', 3000);
+dotenv.config();
+
+const port = parseInt(process.env.PORT) || 3000;
 
 (async () => {
     await api.start(port);
